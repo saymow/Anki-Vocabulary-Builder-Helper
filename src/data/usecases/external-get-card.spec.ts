2b0b69b17a569e0327b3a2fa95ab7442192cd4aa
Gustavo Alves <gustavo_alves2010@yahoo.com.br>
2022-01-01 15:39:51 -0300

    refactor: move sut creation to a factory method

--- src/data/usecases/external-get-card.spec.ts
+++ src/data/usecases/external-get-card.spec.ts
@@ -1,18 +1,34 @@
 import { GetWordInformationService, WordInformation } from '@/data/protocols/external/get-word-information-service'
 import { ExternalGetCard } from './external-get-card'
 
+const makeGetWordInformationServiceStub = (): GetWordInformationService => {
+  class GetWordInformationServiceStub implements GetWordInformationService {
+    async getInformation (word: string): Promise<WordInformation> {
+      return await Promise.resolve({
+        definition: 'any_definition',
+        phrasesExamples: ['any_phrase 1', 'any_phrase 2']
+      })
+    }
+  }
+
+  return new GetWordInformationServiceStub()
+}
+
+type SutTypes = {
+  getWordInformationServiceStub: GetWordInformationService
+  sut: ExternalGetCard
+}
+
+const makeSut = (): SutTypes => {
+  const getWordInformationServiceStub = makeGetWordInformationServiceStub()
+  const sut = new ExternalGetCard(getWordInformationServiceStub)
+
+  return { sut, getWordInformationServiceStub }
+}
+
 describe('ExternalGetCard useCase', () => {
   test('Should call GetWordInformationService with correct value', async () => {
-    class GetWordInformationServiceStub implements GetWordInformationService {
-      async getInformation (word: string): Promise<WordInformation> {
-        return await Promise.resolve({
-          definition: 'any_definition',
-          phrasesExamples: ['any_phrase 1', 'any_phrase 2']
-        })
-      }
-    }
-    const getWordInformationServiceStub = new GetWordInformationServiceStub()
-    const sut = new ExternalGetCard(getWordInformationServiceStub)
+    const { sut, getWordInformationServiceStub } = makeSut()
     const getInformationSpy = jest.spyOn(getWordInformationServiceStub, 'getInformation')
 
     await sut.execute('any_word')
