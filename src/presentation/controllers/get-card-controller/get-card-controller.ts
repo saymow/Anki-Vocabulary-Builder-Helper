64cac90ff9edfd3b16e15600f6a2f8a69a03355c
Gustavo Alves <gustavo_alves2010@yahoo.com.br>
2022-01-01 09:09:01 -0300

    feat: add Controller protocol

--- src/presentation/controllers/get-card-controller/get-card-controller.ts
+++ src/presentation/controllers/get-card-controller/get-card-controller.ts
@@ -1,7 +1,8 @@
+import { Controller } from '../../protocols/controller';
 import { HttpRequest, HttpResponse } from '../../protocols/http';
 import { Validation } from '../../protocols/validation';
 
-export class GetCardController {
+export class GetCardController implements Controller {
   constructor(private readonly validation: Validation) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
