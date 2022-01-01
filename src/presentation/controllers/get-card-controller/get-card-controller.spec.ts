2ba7620809e5532bddcd158e3b8c7a7295841636
Gustavo Alves <gustavo_alves2010@yahoo.com.br>
2022-01-01 09:28:37 -0300

    refactor: move request to a factory helper method

--- src/presentation/controllers/get-card-controller/get-card-controller.spec.ts
+++ src/presentation/controllers/get-card-controller/get-card-controller.spec.ts
@@ -2,6 +2,13 @@ import { badRequest } from '../../helpers/http/http-helper';
 import { MissingParamError } from '../../errors';
 import { Validation } from '../../protocols/validation';
 import { GetCardController } from './get-card-controller';
+import { HttpRequest } from '../../protocols/http';
+
+const makeFakeRequest = (): HttpRequest => ({
+  queryParams: {
+    word: 'any_word',
+  },
+});
 
 const makeValidationStub = (): Validation => {
   class ValidationStub implements Validation {
@@ -29,12 +36,7 @@ describe('GetCard controller', () => {
   test('Should call validation with correct value', async () => {
     const { sut, validationStub } = makeSut();
     const validationSpy = jest.spyOn(validationStub, 'validate');
-
-    await sut.handle({
-      queryParams: {
-        word: 'any_word',
-      },
-    });
+    await sut.handle(makeFakeRequest());
 
     expect(validationSpy).toHaveBeenCalledWith({
       word: 'any_word',
@@ -44,10 +46,7 @@ describe('GetCard controller', () => {
   test('Should return 400 if validation returns an error', async () => {
     const { sut, validationStub } = makeSut();
     jest.spyOn(validationStub, 'validate').mockImplementation(() => new MissingParamError('word'));
-
-    const httpResponse = await sut.handle({
-      queryParams: {},
-    });
+    const httpResponse = await sut.handle(makeFakeRequest());
 
     expect(httpResponse).toEqual(badRequest(new MissingParamError('word')));
   });
