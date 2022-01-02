6939d4d7677cb1c719d8c14628e1f6283992fa34
Gustavo Alves <gustavo_alves2010@yahoo.com.br>
2022-01-02 14:35:49 -0300

    feat: add express and configure app entry

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,3 +1,4 @@
 export const ENV = {
-  WORDS_API_API_KEY: process.env.WORDS_API_API_KEY as string
+  WORDS_API_API_KEY: process.env.WORDS_API_API_KEY as string,
+  PORT: process.env.PORT ?? 3333
 }
---
+++ src/main/server.ts
@@ -0,0 +1,4 @@
+import app from '@/main/config/app'
+import { ENV } from './config/env'
+
+app.listen(ENV.PORT, () => console.log(`Server is up and running on port: ${ENV.PORT}`))
