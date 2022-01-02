44c0098dfb2d554445c0c6ce02be34d3dfb4a0bd
Gustavo Alves <gustavo_alves2010@yahoo.com.br>
2022-01-02 10:34:10 -0300

    feat: ensure WordsApiWordsInformation returns WordInformation on success

--- src/infra/services/words-information/words-api/words-api-words-information.ts
+++ src/infra/services/words-information/words-api/words-api-words-information.ts
@@ -1,18 +1,40 @@
 import { GetWordInformationService, WordInformation } from '@/data/protocols/external/get-word-information-service'
 import axios from 'axios'
 
+type WordsApiResponseResult = {
+  definition: string
+  partOfSpeech: string
+  synonyms?: string[]
+  examples?: string[]
+}
+
+type WordsApiResponse = {
+  results?: WordsApiResponseResult[]
+}
 export class WordsApiWordsInformation implements GetWordInformationService {
   constructor (
     private readonly apiKey: string
-  ) {}
+  ) { }
 
   async getInformation (word: string): Promise<WordInformation | null> {
-    await axios.get(`https://wordsapiv1.p.rapidapi.com/words/${word}`, {
+    const response: WordsApiResponse = await axios.get(`https://wordsapiv1.p.rapidapi.com/words/${word}`, {
       headers: {
         'x-rapidapi-host': 'wordsapiv1.p.rapidapi.com',
         'x-rapidapi-key': this.apiKey
       }
     })
-    return null
+    const emptyWordInformation: WordInformation = {
+      usageExamples: [],
+      meanings: []
+    }
+
+    return (response.results ?? []).reduce<WordInformation>((wordInformation, result) => {
+      const { definition, partOfSpeech, synonyms = [], examples = [] } = result
+
+      wordInformation.usageExamples.push(...examples)
+      wordInformation.meanings.push({ definition, partOfSpeech, synonyms })
+
+      return wordInformation
+    }, emptyWordInformation)
   }
 }
