37bda4032cb6c25ed3f2c1d86f9bbf674dddb354
Gustavo Alves <gustavo_alves2010@yahoo.com.br>
2022-01-02 14:20:15 -0300

    feat: add GetCardDataController factory

---
+++ src/main/factories/controllers/get-card-data-controller-factory.ts
@@ -0,0 +1,8 @@
+import { GetCardDataController } from '@/presentation/controllers/get-card-data-controller/get-card-data-controller'
+import { Controller } from '@/presentation/protocols/controller'
+import { makeExternalGetCardData } from '../usecases/external-get-card-data/external-get-card-data-factory'
+import { makeGetCardDataValidation } from './get-card-data-validation-factory'
+
+export const makeGetCardDataController = (): Controller => {
+  return new GetCardDataController(makeGetCardDataValidation(), makeExternalGetCardData())
+}
