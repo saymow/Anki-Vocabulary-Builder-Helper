c5c8926371c619da669e466d67d2167a0dac749e
Gustavo Alves <gustavo_alves2010@yahoo.com.br>
2022-01-02 14:01:25 -0300

    feat: ensure ValidationComposite returns the first Validation error faced

--- src/validation/validators/validation-composite.ts
+++ src/validation/validators/validation-composite.ts
@@ -4,6 +4,9 @@ export class ValidationComposite implements Validation {
   constructor (private readonly validations: Validation[]) {}
 
   validate (input: any): Error | void {
-    return new Error()
+    for (const validation of this.validations) {
+      const error = validation.validate(input)
+      if (error) return error
+    }
   }
 }
