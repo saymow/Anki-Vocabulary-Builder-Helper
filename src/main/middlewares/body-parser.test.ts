22f3ce6c7cb4e395312f70916e5a7eab6ede7171
Gustavo Alves <gustavo_alves2010@yahoo.com.br>
2022-01-08 13:28:24 -0300

    feat: add bodyParser middleware

---
+++ src/main/middlewares/body-parser.test.ts
@@ -0,0 +1,14 @@
+import app from '@/main/config/app'
+import { Request, Response } from 'express'
+import request from 'supertest'
+
+describe('Body Parser Middleware', () => {
+  test('Should parse body as json', async () => {
+    app.post('/test_body_parser', (req: Request, res: Response) => { res.send(req.body) })
+
+    await request(app)
+      .post('/test_body_parser')
+      .send({ name: 'Gustavo' })
+      .expect({ name: 'Gustavo' })
+  })
+})
