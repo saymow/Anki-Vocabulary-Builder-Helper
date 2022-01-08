cff38dad98d1e5f55cc61c3a83a7d7915c0948d0
Gustavo Alves <gustavo_alves2010@yahoo.com.br>
2022-01-08 13:36:33 -0300

    feat: add contentType middleware - ensure custom content type if need

--- src/main/middlewares/content-type.test.ts
+++ src/main/middlewares/content-type.test.ts
@@ -10,4 +10,15 @@ describe('Content Type Middleware', () => {
       .get('/test_content_type')
       .expect('content-type', /json/)
   })
+
+  test('Should return custom content type if need', async () => {
+    app.get('/test_custom_content_type', (req: Request, res: Response) => {
+      res.type('xml')
+      res.send()
+    })
+
+    await request(app)
+      .get('/test_custom_content_type')
+      .expect('content-type', /xml/)
+  })
 })
