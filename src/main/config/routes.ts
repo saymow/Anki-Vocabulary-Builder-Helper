0e01116d1c0ae8924099433076ff05ae44a6557f
Gustavo Alves <gustavo_alves2010@yahoo.com.br>
2022-01-08 13:46:42 -0300

    feat: configure express router

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,5 +1,15 @@
-import { Express } from 'express'
+import { Express, Router } from 'express'
+import path from 'path'
+import { readdirSync } from 'fs'
 
 export default async (app: Express): Promise<void> => {
+  const router = Router()
+  router.use('/api', router)
+  const namePartsBlackList = ['.test.', '.map']
 
+  readdirSync(path.resolve(__dirname, '..', 'routes')).forEach(async (fileName) => {
+    if (namePartsBlackList.every((namePart) => !fileName.includes(namePart))) {
+      (await import(path.resolve(__dirname, '..', 'routes', fileName))).default(router)
+    }
+  })
 }
