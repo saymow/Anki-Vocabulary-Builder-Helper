da2d87d87fbfd16c8f4b666ccde884ca7a0e9ed7
Gustavo Alves <gustavo_alves2010@yahoo.com.br>
2022-01-09 15:49:44 -0300

    feat: initialize mongo and thereafter app

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export const ENV = {
+  PORT: process.env.PORT ?? 3333,
   WORDS_API_API_KEY: process.env.WORDS_API_API_KEY as string,
-  PORT: process.env.PORT ?? 3333
+  MONGO_URL: process.env.MONGO_URL as string
 }
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,5 +1,12 @@
 import 'module-alias/register'
-import app from '@/main/config/app'
 import { ENV } from './config/env'
+import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
 
-app.listen(ENV.PORT, () => console.log(`Server is up and running on port: ${ENV.PORT}`))
+void (async () => {
+  await MongoHelper.connect(ENV.MONGO_URL)
+  const app = (await import('./config/app')).default
+
+  app.listen(ENV.PORT, () => console.log('Server is up and running on port:', ENV.PORT))
+})().catch((error) => {
+  console.error('Server startup error:', error)
+})
